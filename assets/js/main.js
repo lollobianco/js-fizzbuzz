0ac1eb25dad52c291501f31c70a5991cd0ec6991
lollobianco <loreb2002@gmail.com>
2022-10-21 15:44:30 +0200

    Layout creato

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,28 @@
 
 for (let n = 1; n <= 100; n++){
 
+   const boxesContainer = document.getElementById("boxesContainer");
+   const boxFizzBuzz= `<div class="d-flex align-items-center justify-content-center col-2 box fizzBuzz">FizzBuzz</div>`;
+   const boxBuzz = `<div class="d-flex align-items-center justify-content-center col-2 box buzz">Buzz</div>`;
+   const boxFizz = `<div class="d-flex align-items-center justify-content-center col-2 box fizz">Fizz</div>`;
+   const box = `<div class="d-flex align-items-center justify-content-center col-2 box normal">${n}</div>`;
+
    if (n % 5 == 0 & n % 3 == 0){
 
       console.log(`${n} è sia multiplo di 5 che di 3`)
+      boxesContainer.innerHTML += boxFizzBuzz;
 
       }else if (n % 5 == 0){
-         console.log(`${n} è multiplo di cinque`)
+         console.log(`${n} è multiplo di cinque`)         
+         boxesContainer.innerHTML += boxBuzz;
       }else if (n % 3 == 0){
-         console.log(`${n} è multiplo di tre`)
+         console.log(`${n} è multiplo di tre`)         
+         boxesContainer.innerHTML += boxFizz;
       }else{
-         console.log(n)
+         console.log(n)      
+         boxesContainer.innerHTML += box;
       }
+
+
+
 }
