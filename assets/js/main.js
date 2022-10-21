1ca0b29f23e08421a43cf39308176629d6cb9ec7
lollobianco <loreb2002@gmail.com>
2022-10-21 16:27:17 +0200

    Perfezionamenti estetici

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@
 for (let n = 1; n <= 100; n++){
 
    const boxesContainer = document.getElementById("boxesContainer");
-   const boxFizzBuzz= `<div class="d-flex align-items-center justify-content-center col-2 box fizzBuzz">FizzBuzz</div>`;
-   const boxBuzz = `<div class="d-flex align-items-center justify-content-center col-2 box buzz">Buzz</div>`;
-   const boxFizz = `<div class="d-flex align-items-center justify-content-center col-2 box fizz">Fizz</div>`;
-   const box = `<div class="d-flex align-items-center justify-content-center col-2 box normal">${n}</div>`;
+   const boxFizzBuzz= `<div class="d-flex align-items-center justify-content-center box fizzBuzz">FizzBuzz</div>`;
+   const boxBuzz = `<div class="d-flex align-items-center justify-content-center box buzz">Buzz</div>`;
+   const boxFizz = `<div class="d-flex align-items-center justify-content-center box fizz">Fizz</div>`;
+   const box = `<div class="d-flex align-items-center justify-content-center box normal">${n}</div>`;
 
    if (n % 5 == 0 & n % 3 == 0){
 
